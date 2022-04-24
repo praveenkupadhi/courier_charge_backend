3c5549110eb2ce47fa38a303a98bc4d50ea3b3ea
praveenkupadhi <praveen7438@gmail.com>
2022-04-24 18:43:12 +0530

    final-commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Backend
+
+Note :- both frontend and backend should run simultaneously. (Frontend port: 3000, Backend port: 3001)
+
+In command-line/terminal, type
+
+1. npm i - to download all packages.
+2. npm run start - to start the server.
+
+backend-url: http://localhost:3001/
--- src/controllers/inputDataController.js
+++ src/controllers/inputDataController.js
@@ -9,6 +9,10 @@ router.post("/", async (req, res) => {
 			return res.data.filter((d) => req.body.pincode === d.customer_pincode);
 		});
 
+	if (pincode.length === 0) {
+		return res.status(404).json("Pincode not found");
+	}
+
 	const rate = await axios.get("http://localhost:3001/rates").then((res) => {
 		for (let i = 0; i < res.data.length; i++) {
 			if (
@@ -20,7 +24,20 @@ router.post("/", async (req, res) => {
 		}
 	});
 
-	console.log(pincode, rate);
+	// console.log(pincode, rate);
+
+	let courier_charge = rate.first_half_kilo_rate;
+	req.body.weight -= 0.5;
+
+	while (req.body.weight >= 0.5) {
+		courier_charge += rate.every_additional_half_kilo_rate;
+		req.body.weight -= 0.5;
+	}
+
+	courier_charge +=
+		Math.round(req.body.weight) + rate.every_additional_half_kilo_rate;
+
+	return res.status(200).json(courier_charge);
 });
 
 module.exports = router;
